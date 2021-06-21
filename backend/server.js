88625eb108c954959bb680d9cc226f306d777ea7
GutaAndrei <andrei.guta99@e-uvt.ro>
2021-06-21 21:02:03 +0300

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,22 +20,22 @@ app.use("/api/devices", deviceRoutes);
 app.use("/api/dates", dateRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
-
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
     res.send("API IS RUNNING");
   });
 }
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
